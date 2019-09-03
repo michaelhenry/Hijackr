f1ae304dd6a39adeace78bb8c9fdb51efff62a45
Michael Henry Pantaleon <me@iamkel.net>
2019-09-03 13:14:28 +0900

    Update readme and example.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -2,27 +2,40 @@ import XCTest
 import Hijackr
 
 class Tests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
+  override func setUp() {
+    super.setUp()
+    Hijackr.register()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+    Hijackr.unregister()
+  }
+  
+  func testExample() {
+
+    let request =  URLRequest(url: URL(string: "https://www.google.com")!)
+    let response = Hijackr.Response(statusCode: 200, body: "hello".data(using: .utf8))
+    Hijackr.hijack(request: request, with: response)
+    let ex = expectation(description: "expect to download a content")
+    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+      XCTAssertNotNil(data)
+      XCTAssertEqual(String(data: data!, encoding: .utf8), "hello")
+      ex.fulfill()
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    task.resume()
+    wait(for: [ex], timeout: 2.0)
+    URLProtocol.unregisterClass(Hijackr.self)
+  }
+  
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure() {
+      // Put the code you want to measure the time of here.
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
 }
+
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Hijackr
+# Hijackr🛩
 
 [![CI Status](https://img.shields.io/travis/michaelhenry/Hijackr.svg?style=flat)](https://travis-ci.org/michaelhenry/Hijackr)
 [![Version](https://img.shields.io/cocoapods/v/Hijackr.svg?style=flat)](https://cocoapods.org/pods/Hijackr)
@@ -9,8 +9,6 @@
 
 To run the example project, clone the repo, and run `pod install` from the Example directory first.
 
-## Requirements
-
 ## Installation
 
 Hijackr is available through [CocoaPods](https://cocoapods.org). To install
@@ -18,6 +16,34 @@ it, simply add the following line to your Podfile:
 
 ```ruby
 pod 'Hijackr'
+```
+
+## How to Use
+
+To register:
+
+```swift
+Hijackr.register()
+```
+
+To unregister:
+
+```swift
+Hijackr.unregister()
+```
+
+To hijack a request:
+
+```
+Hijackr.hijack(request: request, with: response)
+```
+
+Example:
+
+```swift
+let request =  URLRequest(url: URL(string: "https://www.google.com")!)
+let response = Hijackr.Response(statusCode: 200, body: "hello".data(using: .utf8))
+Hijackr.hijack(request: request, with: response)
 ```
 
 ## Author
